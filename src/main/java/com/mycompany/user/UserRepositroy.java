51a0ac8ed05f62202eec1b840e9d6a432c191034
javier estrada <estrada.javier.2022@gmail.com>
2021-10-22 16:59:35 -0500

    Feat: CREUD project finsihed successfully

--- src/main/java/com/mycompany/user/UserRepositroy.java
+++ src/main/java/com/mycompany/user/UserRepositroy.java
@@ -3,5 +3,5 @@ package com.mycompany.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepositroy  extends CrudRepository<User, Integer> {
-
+    public Long countById(Integer id);
 }
